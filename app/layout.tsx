57cb5e5a3f1be8482442b1f5d473c5bfa45c59d8
Chunxu Yang <chunxuyang@ucla.edu>
2024-01-18 12:22:40 -0800

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head></head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
