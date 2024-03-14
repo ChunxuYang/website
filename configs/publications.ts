2ec472e483fab789fecad7b2d64bd34bc86230c3
Chunxu Yang <chunxuyang@ucla.edu>
2024-03-13 23:23:36 -0700

    fix: conference name

--- configs/publications.ts
+++ configs/publications.ts
@@ -16,19 +16,17 @@ export const PUBLICATIONS: Publication[] = [
       "A Human-AI Collaborative System to Support Mitosis Assessment in Pathology.",
     authors: [
       "Chunxu Yang",
-      // Mohammad Haeri, Shino Magaki, Neda Zarrin-Khameh, Hongyan Gu, Xiang 'Anthony' Chen.
       "Mohammad Haeri",
       "Shino Magaki",
       "Neda Zarrin-Khameh",
       "Hongyan Gu",
       "Xiang 'Anthony' Chen",
     ],
-    conference: "IUI 2024 (Just accepted)",
+    conference: "IUI 2024 Demos (Just accepted)",
   },
   {
     title:
       "Enhancing Mitosis Quantification and Detection in in Meningiomas with Computational Digital Pathology",
-    // Hongyan Gu, Chunxu Yang, Issa Al-kharouf, Shino Magaki, Nelli Lakis, Christopher Kazu Williams, Sallam Mohammad Alrosan, Ellie Kate Onstott, Wenzhong Yan1, Negar Khanlou, Inma Cobos, Xinhai Robert Zhang, Neda Zarrin-Khameh, Harry V. Vinters, Xiang ‘Anthony’ Chen, Mohammad Haeri.
     authors: [
       "Hongyan Gu",
       "Chunxu Yang",
